fa02d977b46c34c04052fe9cabb36845a6bb5ea2
mikaco04 <166658299+mikaco04@users.noreply.github.com>
2024-04-22 16:05:21 -0400

    finished the game over and game won screen

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 font = pygame.font.Font(None, 36)
 GRAY = (200, 200, 200)
+width = 700
+height = 800
 
 def draw_button(surface, color, x, y, width, height, text, text_color=BLACK):
     
@@ -22,10 +24,11 @@ def draw_button(surface, color, x, y, width, height, text, text_color=BLACK):
     text_rect = text_surface.get_rect(center=(x + width / 2, y + height / 2))
     surface.blit(text_surface, text_rect)
 
+
 def main():
-    
     # CONSTANTS: 
-    background_colour = (255, 255, 255) 
+    pygame.init()
+    background_colour = (255, 255, 255)
 
     width = 700  # in pixels (subject to change)
     height = 800  # in pixels (subject to change)
@@ -50,10 +53,10 @@ def main():
 
     # Game status will switch to false when the user wins or loses
     game_status = menu = True
-    background_picture = pygame.image.load('background.jpeg')
-    background_picture = pygame.transform.scale(background_picture, (width, height))
+    main_background_picture = pygame.image.load('background.jpeg')
+    main_background_picture = pygame.transform.scale(main_background_picture, (width, height))
     mainbuffer = pygame.Surface((width, height))
-
+    game_done = False
     while game_status:
         mainbuffer.fill((255,255,255))
         for event in pygame.event.get(): 
@@ -82,10 +85,9 @@ def main():
                     board = Board(width, height, screen, difficulty)
                     initial_board = board
                     print("Hard button clicked!")
-        
 
         if menu:
-            mainbuffer.blit(background_picture, (0,0))
+            mainbuffer.blit(main_background_picture, (0,0))
             easyButton.draw(mainbuffer)
             normalButton.draw(mainbuffer)
             hardButton.draw(mainbuffer)
@@ -109,18 +111,20 @@ def main():
 
 
         else:
-            screen.fill((255,255,255))
+            if not game_done:
 
-            restart_button_rect = pygame.Rect(0, button_y_location, button_width, button_height)
-            draw_button(screen, GRAY, restart_button_rect.x, restart_button_rect.y, restart_button_rect.width, restart_button_rect.height, "Restart")
-            
-            reset_button_rect = pygame.Rect(button_width, button_y_location, button_width, button_height)
-            draw_button(screen, GRAY, reset_button_rect.x, reset_button_rect.y, reset_button_rect.width, reset_button_rect.height, "Reset")
-            
-            quit_button_rect = pygame.Rect(2 * button_width, button_y_location, button_width, button_height)
-            draw_button(screen, GRAY, quit_button_rect.x, quit_button_rect.y, quit_button_rect.width, quit_button_rect.height, "Quit")
-            
-            board.draw()
+                screen.fill((255,255,255))
+
+                restart_button_rect = pygame.Rect(0, button_y_location, button_width, button_height)
+                draw_button(screen, GRAY, restart_button_rect.x, restart_button_rect.y, restart_button_rect.width, restart_button_rect.height, "Restart")
+
+                reset_button_rect = pygame.Rect(button_width, button_y_location, button_width, button_height)
+                draw_button(screen, GRAY, reset_button_rect.x, reset_button_rect.y, reset_button_rect.width, reset_button_rect.height, "Reset")
+
+                quit_button_rect = pygame.Rect(2 * button_width, button_y_location, button_width, button_height)
+                draw_button(screen, GRAY, quit_button_rect.x, quit_button_rect.y, quit_button_rect.width, quit_button_rect.height, "Quit")
+
+                board.draw()
 
             last_cell = [-1, -1] # this variable will keep track of selected cell
             for event in pygame.event.get():
@@ -154,6 +158,36 @@ def main():
                                             board.cells[row][col].selected = False
                                     board.cells[x][y].selected = True
                                 board.draw()
+
+                        if board.check_board() == True:
+                            game_done = True
+                            game_won = pygame.image.load('wongame.jpg')
+                            game_won = pygame.transform.scale(game_won, (width, height))
+                            screen.blit(game_won, (0,0))
+                            pygame.display.set_caption('Game won!')
+                            pygame.display.flip()
+                            font_type = pygame.font.Font('text.ttf', 50)
+                            message_displayed = font_type.render("Game Won!", True, BLACK)
+                            screen.blit(message_displayed, (210,380))
+                            pygame.display.flip()
+                            print("Game won!")
+
+
+
+                        elif board.check_board() == False:
+                            game_done = True
+                            game_lost = pygame.image.load('background.jpeg')
+                            game_lost = pygame.transform.scale(game_lost, (width, height))
+                            screen.blit(game_lost, (0,0))
+                            pygame.display.set_caption('Game lost :(')
+                            pygame.display.flip()
+                            font_type = pygame.font.Font('text.ttf', 50)
+                            message_displayed = font_type.render("Game Lost :(", True, BLACK)
+                            screen.blit(message_displayed, (190,120))
+                            pygame.display.flip()
+                            print("Game Lost!")
+
+
 
             # Add additional buttons here:
 
